6f8f0b982a83fb20da1255f48d2c9372bd29051b
willardraborg <90839818+willardraborg@users.noreply.github.com>
2022-03-04 13:11:54 +0100

    Update SudokuApplication.java

--- Sudoku/Sudoku/SudokuApplication.java
+++ Sudoku/Sudoku/SudokuApplication.java
@@ -5,23 +5,5 @@ public class SudokuApplication {
         SudokuSolver solver = new SudokuSolver();
         new SudokuGUI(solver);
 
-        // fixa med GitLab
-
-        /*
-        Skriv testfall med JUnit för metoderna i din sudokuklass. Bland testme-
-        toderna ska ingå lösning av ett tomt sudoku, lösning av sudokut i figur 1 samt försök att
-        lösa lämpliga fall av olösliga sudokun. Alla publika metoder ska ha anropats åtminstone
-        en gång i testklassen.
-        Interfacet SudokuSolver ska användas som typ för sudokulösaren i testklassen. Det är bara
-        när sudokulösarobjektet skapas som du ska använda ditt eget klassnamn.
-         */
-
-        // kolla checklista och få allt att stämma där
-
-        // Prova gärna att skapa javadoc-filer och kontrollera att dokumentationen ser bra ut.
-
-        // läs instruktionsdokument för redovisning av labb
-
-        // läs igenom instruktionerna en sista gång
     }
 }
